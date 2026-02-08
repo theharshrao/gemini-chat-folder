ae9ca662a331cb6ea70f49e421354bddef62b795
Harsh Rao <bhupesh9785@gmail.com>
2026-02-08 22:30:35 +0530

    Expose config.js for plug-and-play functionality (Public Backend)

---
+++ config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    SUPABASE_URL: 'https://zqoriuatdgiiowdfmezn.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxb3JpdWF0ZGdpaW93ZGZtZXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1MjYxODIsImV4cCI6MjA4NjEwMjE4Mn0.4_7CwhNsX0JdTdEfxYo-2dwQTvnfRuwv-eljjgMQKYg'
+};
